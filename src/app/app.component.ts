3659054f6dc63469c5f50bed6dc1dba58b2e85cc
Thiago Lima <thiagobr@br.ibm.com>
2017-01-05 01:42:18 -0200

    Creating the Header Class and its object properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 
+export class Header {
+  logo: string;
+  title: string;
+  subtitle: string;
+  date: Date;
+}
+
+const HEADER: Header = { 
+  logo: 'Logo', 
+  title: 'Stores', 
+  subtitle: 'The Ultimate Management For Your Business', 
+  date: new Date()  
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  head = {
-    title: 'app works!'
-  };
+  header = { 
+    logo: HEADER.logo,
+    name: HEADER.title,
+    hp: HEADER.subtitle,
+    date: HEADER.date 
+  }
+    
 }
+
