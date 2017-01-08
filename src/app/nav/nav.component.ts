653c347e51e9a5803fc14478542fafaf64d7746c
Thiago Lima <thiagobr@br.ibm.com>
2017-01-07 23:28:03 -0200

    Adding the first class to nav.component.ts

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export class NavStructure {
+  private home: string;
+  private whoWeAre: string;
+  private showCase: string;
+  private stores: string;
+  private contact: string;
+
+}
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
