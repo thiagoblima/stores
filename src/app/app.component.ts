97e9b90bfcaa184f29d53d59193dd3863ef94203
Thiago Lima <thiagobr@br.ibm.com>
2017-01-08 02:13:47 -0200

    Adding new settings to app.component.ts for reading SASS file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,7 +62,7 @@ header.setDate = new Date();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
