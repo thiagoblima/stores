417ce1be439543d874852d851232fc443c562d0a
Thiago Lima <thiagobr@br.ibm.com>
2017-01-07 22:08:46 -0200

    Adding a new component to the main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent
   ],
   imports: [
     BrowserModule,
