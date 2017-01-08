e496ad73e91644f47faaa9b796a91728d9f1c061
Thiago Lima <thiagobr@br.ibm.com>
2017-01-08 20:29:47 -0200

    Adding the new path to commons folder A025

--- src/app/commons/nav/nav.component.ts
+++ src/app/commons/nav/nav.component.ts
@@ -13,17 +13,17 @@ export class NavStructure {
   public stores: string;
   public contact: string;
 
-  constructor(home, whoWeAre, showCase, stores, contact){
-     this.home = home;
-     this.whoWeAre = whoWeAre;
-     this.showCase = showCase;
-     this.stores = stores;
-     this.contact = contact;
+  constructor(home, whoWeAre, showCase, stores, contact) {
+    this.home = home;
+    this.whoWeAre = whoWeAre;
+    this.showCase = showCase;
+    this.stores = stores;
+    this.contact = contact;
   }
 
 }
 
-const NAV: NavStructure = {
+const nav: NavStructure = {
   home: 'Home',
   whoWeAre: 'Who We Are',
   showCase: 'Show Case',
@@ -39,16 +39,16 @@ const NAV: NavStructure = {
 
 export class NavComponent implements OnInit {
 
-  nav = {
-    home: NAV.home,
-    whoWeAre: NAV.whoWeAre,
-    showCase: NAV.showCase,
-    stores: NAV.stores,
-    contact: NAV.contact
-  }
+  links = [
+    { menu: nav.home, alt: 'Home', id: 'link-1' },
+    { menu: nav.whoWeAre, alt: 'Who We Are', id: 'link-2' },
+    { menu: nav.showCase, alt: 'Show Case', id: 'link-3' },
+    { menu: nav.stores, alt: 'Stores', id: 'link-4' },
+    { menu: nav.contact, alt: 'Contact', id: 'link-5' }
+  ];
 
   ngOnInit() {
-    
+
   }
 
 }
