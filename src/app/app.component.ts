115c9c39d95761a562a9ae206b16003f24cf150c
Thiago Lima <thiagobr@br.ibm.com>
2017-01-07 23:54:37 -0200

    Adding a new instance of the object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 export class Header {
-  public logo: string;
-  public title: string;
-  public subtitle: string;
-  public date: Date;
+  private logo: string;
+  private title: string;
+  private subtitle: string;
+  private date: Date;
 
   get getLogo(): string {
     return this.logo;
@@ -13,11 +13,20 @@ export class Header {
   set setLogo(logo){
     this.logo = logo;
   }
+
+  get getTitle(): string {
+    return this.title;
+  }
+
+  set setTitle(title){
+    this.title = title;
+  }
 }
 
 const header = new Header();
 
 header.setLogo = 'Logo';
+header.setTitle = 'Stores';
 
 /*const HEADER: Header = { 
   title: 'Stores', 
@@ -35,7 +44,8 @@ header.setLogo = 'Logo';
 
 export class AppComponent {
   header = { 
-    logo: header.getLogo 
+    logo: header.getLogo,
+    title: header.getTitle 
   }
     
 }
