0b7fb33890b5cd9b5e0516c465fe27d5d7cfa821
Thiago Lima <thiagobr@br.ibm.com>
2017-01-08 15:21:24 -0200

    Adding the new path to commons folder A018

---
+++ src/app/commons/nav/nav.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+export class NavStructure {
+
+  /** 
+   * @name: NavStrucutre 
+   * @description: Object instances written here 
+   **/
+
+  public home: string;
+  public whoWeAre: string;
+  public showCase: string;
+  public stores: string;
+  public contact: string;
+
+  constructor(home, whoWeAre, showCase, stores, contact){
+     this.home = home;
+     this.whoWeAre = whoWeAre;
+     this.showCase = showCase;
+     this.stores = stores;
+     this.contact = contact;
+  }
+
+}
+
+const NAV: NavStructure = {
+  home: 'Home',
+  whoWeAre: 'Who We Are',
+  showCase: 'Show Case',
+  stores: 'Stores',
+  contact: 'Contact'
+};
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+
+export class NavComponent implements OnInit {
+
+  nav = {
+    home: NAV.home,
+    whoWeAre: NAV.whoWeAre,
+    showCase: NAV.showCase,
+    stores: NAV.stores,
+    contact: NAV.contact
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
