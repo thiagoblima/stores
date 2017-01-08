dc842893dc3312bc1d1a9ea06434b9e343cbaa1c
Thiago Lima <thiagobr@br.ibm.com>
2017-01-08 05:49:35 -0200

    Adding a new component to app module: header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
