ce85e5cfe15c691deb18fa7ea49db3861162a793
Thiago Lima <thiagobr@br.ibm.com>
2017-01-08 17:59:30 -0200

    Adding the new path to commons folder A021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,71 +2,7 @@ import { Component } from '@angular/core';
 import { NavComponent } from './commons/nav/nav.component';
 import { HeaderComponent } from './commons/header/header.component';
 
-export class Header {
 
-  /** 
-   * @name: Header
-   * @description: Object instances written here 
-   **/
-
-  private logo: string;
-  private title: string;
-  private subtitle: string;
-  private date: Date;
-
-  constructor(logo, title, subtitle, date) {
-    this.logo = logo;
-    this.title = title;
-    this.subtitle = subtitle;
-    this.date = date;
-  }
-
-  public get getLogo(): string {
-    return this.logo;
-  }
-  
-  public set setLogo(logo){
-    this.logo = logo;
-  }
-
-  public get getTitle(): string {
-    return this.title;
-  }
-
-  public set setTitle(title){
-    this.title = title;
-  }
-
-  public get getSubtitle(): string {
-    return this.subtitle;
-  }
-
-  public set setSubtitle(subtitle){
-    this.subtitle = subtitle;
-  }
-
-  public get getDate(): Date {
-    return new Date();
-  }
-
-  public set setDate(date) {
-    this.date = date;
-  }
-
-}
-
-/**
-  * @name: header 
-  * @param: logo, title, subtitle, date 
-  * @description: Creating a new element from Header Class 
-  **/
-
-const header = new Header('logo', 'title', 'subtitle', new Date());
-
-header.setLogo = 'Logo';
-header.setTitle = 'Stores';
-header.setSubtitle = 'The Ultimate Management For Your Business';
-header.setDate = new Date();
 
 @Component({
   selector: 'app-root',
@@ -75,11 +11,5 @@ header.setDate = new Date();
 })
 
 export class AppComponent {
-  header = { 
-    logo: header.getLogo,
-    title: header.getTitle,
-    subtitle: header.getSubtitle,
-    date: header.getDate
-  }
-    
+   
 }
