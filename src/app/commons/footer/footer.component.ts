b73e9e33789639ec5b9e04da495cba7a5acf6654
Thiago Lima <thiagobr@br.ibm.com>
2017-01-09 16:21:27 -0200

    Adding the new class for footer

--- src/app/commons/footer/footer.component.ts
+++ src/app/commons/footer/footer.component.ts
@@ -1,4 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+
+export class Footer {
+
+  /** 
+    * @name: Footer
+    * @description: Object instances written here 
+    **/
+
+  private logo: string;
+  private rights: string;
+  private message: string;
+
+  constructor(logo, rights, message) {
+    this.logo = logo;
+    this.rights = rights;
+    this.message = message;
+ }
+
+  public get getLogo(): string {
+    return this.logo;
+  }
+
+  public set setLogo(logo) {
+    this.logo = logo;
+  }
+
+  public get getRights(): string {
+    return this.rights;
+  }
+
+  public set setRights(rights) {
+    this.rights = rights
+  }
+
+  public get getMessage(): string {
+    return this.message;
+  }
+
+  public set setMessage(message) {
+    this.message = message;
+  }
+
+}
 
 @Component({
   selector: 'app-footer',
