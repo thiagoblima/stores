8952e08f6a5b6499579d2e39adfd6d2842d037dc
Thiago Lima <thiagobr@br.ibm.com>
2017-01-10 00:35:18 -0200

    Adding footer component A002

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './commons/nav/nav.component';
 import { HeaderComponent } from './commons/header/header.component';
+import { FooterComponent } from './commons/footer/footer.component';
 
 
 
