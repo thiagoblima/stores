f572ede20e0406ab4fcd3e4cb6d94f1bac6b959f
Thiago Lima <thiagobr@br.ibm.com>
2017-01-21 23:46:50 -0200

    Adding a new interface and class for Container Component

--- src/app/commons/container/container.component.ts
+++ src/app/commons/container/container.component.ts
@@ -1,10 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ContainerConfig {
+
+  /**
+   * @description: Instances of objects
+   */
+
+}
+
+export class Container implements ContainerConfig {
+    
+    /** 
+    * @name: Container
+    * @description: Object instances written here 
+    **/
+
+    constructor(){
+
+    }
+
+
+}
+
+
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
+
 export class ContainerComponent implements OnInit {
 
   constructor() { }
