a870659cafa2779c3dfcde7bb77e7ee625d399f7
Thiago <thiagobr@br.ibm.com>
2017-02-23 22:59:50 -0300

    Container Development A006

--- src/app/commons/container/container.component.ts
+++ src/app/commons/container/container.component.ts
@@ -80,6 +80,9 @@ export class Container implements ContainerConfig {
 const store1: ContainerConfig = new Container('path1', 'hero1', 'caption1');
 const store2: ContainerConfig = new Container('path2', 'hero2', 'caption2');
 const store3: ContainerConfig = new Container('path3', 'hero3', 'caption3');
+const store4: ContainerConfig = new Container('path4', 'hero4', 'caption4');
+const store5: ContainerConfig = new Container('path5', 'hero5', 'caption5');
+const store6: ContainerConfig = new Container('path6', 'hero6', 'caption6');
 
 // store 1 settings
 
@@ -99,6 +102,24 @@ store3.setStoreImg = 'Image3';
 store3.setHero = 'Store 3';
 store3.setCaption = 'Skate shop, the best shapes, wheels and bearings';
 
+// store 4 settings
+
+store4.setStoreImg = 'Image4';
+store4.setHero = 'Store 4';
+store4.setCaption = 'Clothes store, the best dresses around you';
+
+// store 5 settings
+
+store5.setStoreImg = 'Image5';
+store5.setHero = 'Store 5';
+store5.setCaption = 'Vine store, the best french and italian vines';
+
+// store 6 settings
+
+store6.setStoreImg = 'Image6';
+store6.setHero = 'Store 6';
+store6.setCaption = 'Vegan restaurant, come and know the best one';
+
 
 @Component({
   selector: 'app-container',
@@ -111,7 +132,10 @@ export class ContainerComponent implements OnInit {
   stores = [
     { image: store1.getStoreImg, hero: store1.getHero,caption: store1.getCaption },
     { image: store2.getStoreImg, hero: store2.getHero,caption: store2.getCaption },
-    { image: store3.getStoreImg, hero: store3.getHero,caption: store3.getCaption }
+    { image: store3.getStoreImg, hero: store3.getHero,caption: store3.getCaption },
+    { image: store4.getStoreImg, hero: store4.getHero,caption: store4.getCaption },
+    { image: store5.getStoreImg, hero: store5.getHero,caption: store5.getCaption },
+    { image: store6.getStoreImg, hero: store6.getHero,caption: store6.getCaption }
   ];
 
   ngOnInit() {
