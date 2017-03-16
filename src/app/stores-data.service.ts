ef81ea6f1d449dd333e4713a41bb7317dcf05373
Thiago <thiagobr@br.ibm.com>
2017-03-15 22:46:36 -0300

    Service Provider A005

--- src/app/stores-data.service.ts
+++ src/app/stores-data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
+import { Response, RequestOptions, Headers, Request, RequestMethod } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StoresDataService {
@@ -7,7 +10,11 @@ export class StoresDataService {
   constructor(private http: Http) { }
 
   private getStores(){
-    
+
+    return this.http.get('app/cities.json')    
+      .map(res => res.json())
+      .subscribe(res => console.log(res, "Subscribe Response"))
+      
   }
 
 }
