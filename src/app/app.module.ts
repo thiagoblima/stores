7e27547b3bc2ae2c5f72ef891a5f0cea31f9c6c3
Thiago <thiagobr@br.ibm.com>
2017-03-15 22:20:19 -0300

    Service Provider A001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './commons/nav/nav.component';
 import { HeaderComponent } from './commons/header/header.component';
 import { FooterComponent } from './commons/footer/footer.component';
 import { ContainerComponent } from './commons/container/container.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { ContainerComponent } from './commons/container/container.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HttpModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
