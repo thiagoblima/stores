dbb7fbdca780ee17cdea1fb08da59d5b28e17289
Thiago <thiagobr@br.ibm.com>
2017-03-16 03:28:32 -0300

    Service Provider A021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { NavComponent } from './commons/nav/nav.component';
 import { HeaderComponent } from './commons/header/header.component';
 import { FooterComponent } from './commons/footer/footer.component';
@@ -6,11 +6,13 @@ import { ContainerComponent } from './commons/container/container.component';
 import { StoresDataService } from './stores-data.service';
 
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [ StoresDataService ]
+  providers:[ StoresDataService ]
 })
 
 export class AppComponent {
