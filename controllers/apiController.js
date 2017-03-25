ab38df8623143d1202d66f46ccb4bb9f139ffec4
Thiago <thiagobr@br.ibm.com>
2017-03-25 00:53:25 -0300

    BackEnd Strucutre A018

---
+++ controllers/apiController.js
@@ -0,0 +1,81 @@
+/**
+ * @author: Thiago Lima
+ * @description: REST API controller, responsible for CRUD Todo services.
+ * @module: App - Adding Seed Data
+ */
+
+
+const Users = require('../models/userModel');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/api/users/:username', (req, res) => {
+
+        Todos.find({ username: req.params.username }, (err, users) => {
+            if (err) throw err;
+
+            res.send(users);
+        });
+
+    });
+
+    app.get('/api/user/:id', (req, res) => {
+
+        Todos.findById({ _id: req.params.id }, (err, user) => {
+            if (err) throw err;
+
+            res.send(user);
+        });
+
+    });
+
+    app.post('/api/user', (req, res) => {
+
+        if (req.body.id) {
+            Users.findByIdAndUpdate(req.body.id,
+                {
+                    username: req.body.username,
+                    lastname: req.body.lastname,
+                    photo: req.body.photo,
+                    isAdmin: req.body.isAdmin,
+                    hasAttachment: req.body.hasAttachment
+
+                }, (err, todo) => {
+                    if (err) throw err;
+
+                    res.send('Success');
+                });
+        }
+
+        else {
+
+            let newTodo = Todos({
+                username: req.body.username,
+                lastname: req.body.lastname,
+                photo: req.body.photo,
+                isAdmin: req.body.isAdmin,
+                hasAttachment: req.body.hasAttachment
+            });
+            newTodo.save((err) => {
+                if (err) throw err;
+                res.send('Success');
+            });
+
+        }
+
+    });
+
+    app.delete('/api/user', (req, res) => {
+
+        Todos.findByIdAndRemove(req.body.id, (err) => {
+            if (err) throw err;
+            res.send('Success');
+        })
+
+    });
+
+}
