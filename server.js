77d3a258f59c3d0d41f1ba5faaf6dc97e31a0e6d
Thiago <thiagobr@br.ibm.com>
2017-03-25 00:24:07 -0300

    BackEnd Strucutre A014

---
+++ server.js
@@ -0,0 +1,26 @@
+/**
+ * @author: Thiago Lima
+ * @description: Stores Case App Server.
+ * @module: Server { app }
+ */
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config');
+const setupController = require('./controllers/setupController');
+const apiController = require('./controllers/apiController');
+
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
+setupController(app);
+apiController(app);
+
+app.listen(port);
+
+module.exports = app;
