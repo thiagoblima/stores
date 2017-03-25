c8d4da9544412f64116bce4ccd22de6e2521fa95
Thiago <thiagobr@br.ibm.com>
2017-03-25 13:08:22 -0300

    BackEnd Strucutre A030

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const apiController = require('./controllers/apiController');
 
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public/dist'));
 
 app.set('view engine', 'ejs');
 
