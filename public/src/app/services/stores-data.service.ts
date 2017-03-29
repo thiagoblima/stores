ede8aab574dcc6f7f78418b86259d6d63eee9e2d
Thiago <thiagobr@br.ibm.com>
2017-03-29 01:02:01 -0300

    Migrating to Angular 4 A022

--- public/src/app/services/stores-data.service.ts
+++ public/src/app/services/stores-data.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class StoresDataService {
 
-  private stores: StoresData <string>;
+  private stores: StoresData;
   private headers = new Headers({'Content-Type': 'application/json'});
   private api: string = '../../assets/services/stores.json';
 
@@ -14,11 +14,11 @@ export class StoresDataService {
     this.stores;
   }
 
-  public getStores(): Promise <StoresData <string>[]> {
+  public getStores(): Promise <StoresData[]> {
 
     return this.http.get(this.api)
       .toPromise()
-      .then(response => response.json() as StoresData<String>[])
+      .then(response => response.json() as StoresData[])
 
   }
 
