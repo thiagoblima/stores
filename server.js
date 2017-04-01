abb84c53839f6d85c51a0d46a30581ba73707938
Thiago <thiagobr@br.ibm.com>
2017-03-31 23:18:07 -0300

    User Services A018

--- server.js
+++ server.js
@@ -15,15 +15,12 @@ const apiController = require('./controllers/apiController');
 const port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public/dist'));
-
 app.use(morgan('dev'));  
-
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 
 setupController(app);
-
 apiController(app);
 
 app.listen(port);
