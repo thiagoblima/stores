7577cd6dfdd7a9786357a802ce89a41e92cebc14
Thiago <thiagobr@br.ibm.com>
2017-04-02 22:33:27 -0300

    Migrating JWT BackEnd A006

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-/**
- * @author: Thiago Lima
- * @description: Stores Case App Server.
- * @module: Server { app }
- */
-
+const config = require('./config/database'); 
+const apiRoutes = require('./controllers/userApi')
 const express = require('express');
-const morgan = require('morgan'); 
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
-const config = require('./config');
-const setupController = require('./controllers/setupController');
-const apiController = require('./controllers/apiController');
-
+const passport = require('passport');
 const port = process.env.PORT || 3000;
 
+
+// get our request parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use our root directory to read index.html
 app.use('/', express.static(__dirname + '/public/dist'));
-app.use(morgan('dev'));  
-app.set('view engine', 'ejs');
 
-mongoose.connect(config.getDbConnectionString());
+// demo Route (GET http://localhost:8080)
+app.get('/', (req, res) => {
+    res.send('The API is at http://localhost:' + port + '/api');
+});
 
-setupController(app);
-apiController(app);
+// log to console
+app.use(morgan('dev'));
 
+// Start the server
 app.listen(port);
+console.log('App running at: http://localhost:' + port);
 
+
+// connect to database
+mongoose.connect(config.database);
+
+// pass users' Api 
+apiRoutes(app);
+
+// exporting the app 
 module.exports = app;
