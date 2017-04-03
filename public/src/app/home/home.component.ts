42aa72f3e7a431b342d194ef111def123189db1b
Thiago <thiagobr@br.ibm.com>
2017-04-02 21:17:15 -0300

    Home Component A007

---
+++ public/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavComponent } from '../commons/nav/nav.component';
+import { HeaderComponent } from '../commons/header/header.component';
+import { FooterComponent } from '../commons/footer/footer.component';
+import { SideBarComponent } from '../commons/sidebar/sidebar.component';
+import { StoresDataService } from '../services/stores-data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  providers:[ StoresDataService ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
