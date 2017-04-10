bf3a500c519230bb5c1d82a35cab067ded7664a8
Thiago <thiagobr@br.ibm.com>
2017-04-09 23:51:07 -0300

    Creating Login Component A007

---
+++ public/src/app/services/authentication.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthenticationServiceService } from './authentication-service.service';
+
+describe('AuthenticationServiceService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthenticationServiceService]
+    });
+  });
+
+  it('should ...', inject([AuthenticationServiceService], (service: AuthenticationServiceService) => {
+    expect(service).toBeTruthy();
+  }));
+});
