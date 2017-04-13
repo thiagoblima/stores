1622f66ed0a135332c5d01bdad1ec927bd363fc6
Thiago <thiagobr@br.ibm.com>
2017-04-13 00:59:25 -0300

    Auth Service A004

--- public/src/app/services/auth-service/authentication.service.ts
+++ public/src/app/services/auth-service/authentication.service.ts
@@ -9,7 +9,7 @@ export class AuthenticationService {
 
     constructor(private http: Http) {
         // set token if saved in local storage
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
         this.token = currentUser && currentUser.token;
     }
 
