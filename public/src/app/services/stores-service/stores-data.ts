7c92c8c3cc7fca2631ea3f821cb953181efd0c5f
Thiago <thiagobr@br.ibm.com>
2017-04-12 21:55:51 -0300

    Code Review A012

---
+++ public/src/app/services/stores-service/stores-data.ts
@@ -0,0 +1,25 @@
+/**
+ * @author: Thiago Lima
+ * 
+ * @description: Generic class which's used to 
+ * define its typings. It's being imported in 
+ * { StoresDataService }
+ * 
+ */
+
+interface StoresDataSettings<T> {
+
+    id: String;
+    name: String;
+    state: String;
+    description: String;
+}
+
+export class StoresData implements StoresDataSettings<String> {
+
+    public id;
+    public name;
+    public state;
+    public description;
+
+}
