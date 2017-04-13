cec54af16a0e9f0c87c7d023c23873dc73233154
Thiago <thiagobr@br.ibm.com>
2017-04-12 21:47:32 -0300

    Code Review A004

--- public/src/app/services/authentication.service.spec.ts
+++ public/src/app/services/authentication.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { AuthenticationServiceService } from './authentication-service.service';
+import { AuthenticationServiceService } from './authentication.service';
 
 describe('AuthenticationServiceService', () => {
   beforeEach(() => {
