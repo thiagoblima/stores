081d16bfbbb3d6c3b870e3a50576479bda8baba7
Thiago <thiagobr@br.ibm.com>
2017-04-14 15:34:07 -0300

    Services Scaffolding A017

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { NavComponent } from '../commons/nav/nav.component';
 import { HeaderComponent } from '../commons/header/header.component';
 import { FooterComponent } from '../commons/footer/footer.component';
 import { SideBarComponent } from '../commons/sidebar/sidebar.component';
-import { StoresDataService } from '../services/stores-service/stores-data.service';
+import { StoresDataService } from '../services/stores/stores-data.service';
 
 @Component({
   selector: 'app-home',
