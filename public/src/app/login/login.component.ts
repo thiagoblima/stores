d31e230cdae25c9e6237581eb36d722426c78d1e
Thiago <thiagobr@br.ibm.com>
2017-04-14 15:34:27 -0300

    Services Scaffolding A018

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AuthenticationService } from '../services/auth-service/authentication.service';
+import { AuthenticationService } from '../services/auth/authentication.service';
 
 @Component({
     moduleId: module.id,
