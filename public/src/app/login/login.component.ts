e31c9f431e8163a49c64f125d1731a986ea49856
Thiago <thiagobr@br.ibm.com>
2017-04-14 16:43:21 -0300

    Services Scaffolding A026

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { AuthenticationService } from '../services/auth/authentication.service';
 
 @Component({
