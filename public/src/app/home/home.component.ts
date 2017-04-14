0ebe986d358ebd85b2696f8870008e15d0ab5418
Thiago <thiagobr@br.ibm.com>
2017-04-14 18:55:07 -0300

    Services Scaffolding A031

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -4,18 +4,28 @@ import { HeaderComponent } from '../commons/header/header.component';
 import { FooterComponent } from '../commons/footer/footer.component';
 import { SideBarComponent } from '../commons/sidebar/sidebar.component';
 import { StoresDataService } from '../services/stores/stores-data.service';
+import { User } from '../services/models/index';
+import { UserService } from '../services/auth/index';
+
 
 @Component({
+  moduleId: module.id,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   providers:[ StoresDataService ]
 })
 export class HomeComponent implements OnInit {
+    users: User[] = [];
 
-  constructor() { }
+    constructor(private userService: UserService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // get users from secure api end point
+        this.userService.getUsers()
+            .subscribe(users => {
+                this.users = users;
+            });
+    }
 
 }
