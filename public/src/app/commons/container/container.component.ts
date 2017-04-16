ed4021f27afd3a78f1d23ba45b0ee85137a6b128
Thiago <thiagobr@br.ibm.com>
2017-04-15 23:15:54 -0300

    CSS Design Code Review A031

---
+++ public/src/app/commons/container/container.component.ts
@@ -0,0 +1,39 @@
+import { Component, NgModule, OnInit, trigger, transition, style, animate, state } from '@angular/core';
+import { StoresDataService } from '../../services/stores/stores-data.service';
+import { Http } from '@angular/http';
+
+
+@Component({
+  selector: 'app-sidebar',
+  animations: [
+    trigger(
+      'myAnimation',
+      [
+        transition(
+          ':enter', [
+            style({ backgroundColor:'transparent', color: '#F4F2F4', opacity: 1 }),
+            animate('800ms')
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({opacity: 0 }),
+            animate('800ms')
+          ]
+        )
+      ]
+    )
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  providers: [ StoresDataService ]
+})
+
+
+export class ContainerComponent {
+  private stores;
+  constructor(private _storesDataService: StoresDataService){
+    this.stores = _storesDataService.getStores();
+  }
+
+}
