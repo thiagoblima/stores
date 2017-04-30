8ca73eebc58b12d332138fb8444871266e725f17
Thiago <thiagobr@br.ibm.com>
2017-04-29 23:11:34 -0300

    User REST API Enchancement A001

--- public/src/app/services/models/user.ts
+++ public/src/app/services/models/user.ts
@@ -1,8 +1,31 @@
-export class User {
+/**
+ * @author: Thiago Limaall
+ * 
+ * @description: Generic class which's used to 
+ * define its typings. It's being imported in 
+ * { UserService }
+ * 
+ */
+
+interface UserData <T, Z> {
+
+    id: string;
     username: string;
-    password: string;
     email: string;
     firstname: string;
     lastname: string;
     age: number;
+    
+}
+
+
+
+export class User implements UserData <String, Number> {
+    public id;
+    public username;
+    public password;
+    public email;
+    public firstname;
+    public lastname;
+    public age;
 }
