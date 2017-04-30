f2da05204db9ab258b52c37157d3b4acd3032491
Thiago <thiagobr@br.ibm.com>
2017-04-30 19:59:36 -0300

    User REST API Enchancement A039

--- public/src/app/app.routing.ts
+++ public/src/app/app.routing.ts
@@ -5,11 +5,11 @@ import { AuthGuard } from './services/guards/index';
 
 
 const appRoutes: Routes = [
-    { path: 'login', component: LoginComponent },
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-
+    { path: 'login', component: LoginComponent },
+ 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
-
+ 
 export const routing = RouterModule.forRoot(appRoutes);
