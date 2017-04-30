26779614e780c18db634d065cf72a434afbb7580
Thiago <thiagobr@br.ibm.com>
2017-04-30 20:00:47 -0300

    User REST API Enchancement A042

--- public/src/app/services/auth/user.service.ts
+++ public/src/app/services/auth/user.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
 
-import { AuthenticationService } from '../auth/index';
 import { User } from '../../models/index';
 
 @Injectable()
