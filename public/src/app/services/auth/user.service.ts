34f4c31e1f840f17692df3bcae02bdc9d6392055
Thiago <thiagobr@br.ibm.com>
2017-04-29 23:38:01 -0300

    User REST API Enchancement A004

--- public/src/app/services/auth/user.service.ts
+++ public/src/app/services/auth/user.service.ts
@@ -8,18 +8,36 @@ import { User } from '../models/index';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private http: Http,
-        private authenticationService: AuthenticationService) {
+    constructor(private http: Http) { }
+
+    getAll() {
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
     }
 
-    getUsers(): Observable<User[]> {
-        // add authorization header with jwt token
-        let headers = new Headers({ 'Authorization': this.authenticationService.token });
-        let options = new RequestOptions({ headers: headers });
+    getById(id: number) {
+        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
 
-        // get users from api
-        return this.http.get('/api/users', options)
-            .map((response: Response) => response.json());
+    create(user: User) {
+        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    private jwt() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
     }
 }
