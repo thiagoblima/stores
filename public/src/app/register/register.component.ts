e390f69340e2c4dd03512d999e3c6a315604f803
Thiago <thiagobr@br.ibm.com>
2017-05-01 02:05:12 -0300

    Register Component A011

--- public/src/app/register/register.component.ts
+++ public/src/app/register/register.component.ts
@@ -1,15 +1,44 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AlertService, UserService } from '../services/auth/index';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  loading = false;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private alertService: AlertService) { }
+
+  register() {
+    this.loading = true;
+    this.userService.create(this.model)
+      .subscribe(
+      data => {
+        this.alertService.success('Registration successful', true);
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.alertService.error(error);
+        this.loading = false;
+      });
+  }
+
 
   ngOnInit() {
   }
 
 }
+
+
+
+
+
