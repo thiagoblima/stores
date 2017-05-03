7fcdf7d29608f9b4cda18e7d04eb7ea2f2cea94f
Thiago <thiagobr@br.ibm.com>
2017-05-03 00:32:09 -0300

    Stores Component A008

---
+++ public/src/app/stores/stores.component.ts
@@ -0,0 +1,53 @@
+import { Component, NgModule, OnInit, trigger, transition, style, animate, state } from '@angular/core';
+import { StoresDataService } from '../../services/stores/stores-data.service';
+import { Http } from '@angular/http';
+import { UserService } from '../../services/auth/index';
+import { User } from '../../models/index';
+
+@Component({
+  selector: 'app-container',
+  animations: [
+    trigger(
+      'myAnimation',
+      [
+        transition(
+          ':enter', [
+            style({ backgroundColor:'transparent', color: '#F4F2F4', opacity: 1 }),
+            animate('800ms')
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({opacity: 0 }),
+            animate('800ms')
+          ]
+        )
+      ]
+    )
+  ],
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+  providers: [ StoresDataService ]
+})
+
+
+export class ContainerComponent {
+  public currentUser: User;
+  public message: string = '';
+  private stores;
+
+
+  constructor(private _storesDataService: StoresDataService, private userService: UserService){
+     this.stores = _storesDataService.getStores();
+     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  private getUserInfo(){
+        this.userService.getUserInfo().subscribe(message => { this.message = message; })
+    }
+
+    ngOnInit(){
+      this.getUserInfo();
+    }
+
+}
