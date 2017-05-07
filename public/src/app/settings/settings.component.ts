926ed4f16668cb94c5814b7e03503c5d3b023941
Thiago <thiagobr@br.ibm.com>
2017-05-06 22:39:41 -0300

    Settings Component Structure A004

--- public/src/app/settings/settings.component.ts
+++ public/src/app/settings/settings.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NavComponent } from '../commons/nav/nav.component';
+import { HeaderComponent } from '../commons/header/header.component';
+import { FooterComponent } from '../commons/footer/footer.component';
+import { StoresComponent } from '../stores/stores.component';
+import { User } from '../models/index';
+import { UserService } from '../services/auth/index';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+    users: User[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
 
 }
