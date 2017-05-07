94ca1b860e44a7e8bf56651a85f08e455e4d673f
Thiago <thiagobr@br.ibm.com>
2017-05-07 03:21:19 -0300

    Settings Component Structure A010

--- public/src/app/settings/settings.component.ts
+++ public/src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit, trigger, transition, style, animate, state } from '@angular/core';
 import { NavComponent } from '../commons/nav/nav.component';
 import { HeaderComponent } from '../commons/header/header.component';
 import { FooterComponent } from '../commons/footer/footer.component';
@@ -9,28 +9,47 @@ import { UserService } from '../services/auth/index';
 @Component({
   moduleId: module.id,
   selector: 'app-settings',
+  animations: [
+    trigger(
+      'myAnimation',
+      [
+        transition(
+          ':enter', [
+            style({ backgroundColor: 'transparent', color: '#F4F2F4', opacity: 1 }),
+            animate('800ms')
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({ opacity: 0 }),
+            animate('800ms')
+          ]
+        )
+      ]
+    )
+  ],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
 
   currentUser: User;
-    users: User[] = [];
+  users: User[] = [];
 
-    constructor(private userService: UserService) {
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    }
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
-    ngOnInit() {
-        this.loadAllUsers();
-    }
+  ngOnInit() {
+    this.loadAllUsers();
+  }
 
-    deleteUser(id: number) {
-        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
-    }
+  deleteUser(id: number) {
+    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+  }
 
-    private loadAllUsers() {
-        this.userService.getAll().subscribe(users => { this.users = users; });
-    }
+  private loadAllUsers() {
+    this.userService.getAll().subscribe(users => { this.users = users; });
+  }
 
 }
