ae238202fb8fd174e01306f9c15f675ec239fd36
Thiago <thiagobr@br.ibm.com>
2017-05-08 23:40:11 -0300

    User By Id A012

--- public/src/app/settings/user/user.component.ts
+++ public/src/app/settings/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit, trigger, transition, style, animate, state } from '@angular/core';
+import { NavComponent } from '../../commons/nav/nav.component';
+import { HeaderComponent } from '../../commons/header/header.component';
+import { FooterComponent } from '../../commons/footer/footer.component';
+import { StoresComponent } from '../../stores/stores.component';
+import { User } from '../../models/index';
+import { UserService } from '../../services/auth/index';
 
 @Component({
   selector: 'app-user',
