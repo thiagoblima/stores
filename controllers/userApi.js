47f950d789e5137c6669eb567119ad6dbbe8a46f
Thiago <thiagobr@br.ibm.com>
2017-05-09 19:57:34 -0300

    User By Id A029

--- controllers/userApi.js
+++ controllers/userApi.js
@@ -156,7 +156,7 @@ module.exports = (app) => {
                     if (!user) {
                         return res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
                     } else {
-                        res.status(200).json({ success: true, msg: 'User was successfully found', user: user });
+                        res.status(200).json({ success: true, msg: 'User was successfully found', data: user });
                     }
                 });
 
