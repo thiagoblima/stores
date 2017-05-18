c6fecb061a2913f8b31611b6836eb61ed772c445
Thiago <thiagobr@br.ibm.com>
2017-05-18 19:55:17 -0300

    Creating Stores REST API A004

---
+++ models/stores.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let storesSchema = new Schema({
+  store_code: String,
+  store_name: { type: String, required: true, unique: true },
+  store_image: String,
+  store_phone: Number,
+  store_country: String,
+  store_address: String,
+  created_at: Date,
+  updated_at: Date
+});
+
+
+
+
+storesSchema.pre('save', (next) => {
+
+  let currentDate = new Date();
+  
+  this.updated_at = currentDate;
+
+  this.store_code = 'BOM06-' + Math.floor((Math.random() * 999) + 99);
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+
+let Stores = mongoose.model('Stores', storesSchema);
+
+module.exports = Stores;
