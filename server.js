d5283ea7527e2fd3e18be1bde79f3a0f369d1356
Thiago <thiagobr@br.ibm.com>
2017-05-18 21:21:07 -0300

    Creating Stores REST API A011

--- server.js
+++ server.js
@@ -10,33 +10,23 @@ const passport = require('passport');
 const port = process.env.PORT || 3000;
 
 
-// get our request parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// use our root directory to read index.html
 app.use('/', express.static(__dirname + '/public/dist'));
 
-// demo Route (GET http://localhost:8080)
 app.get('/', (req, res) => {
     res.send('The API is at http://localhost:' + port + '/api');
 });
 
-// log to console
 app.use(morgan('dev'));
 
-// Start the server
 app.listen(port);
 console.log('App running at: http://localhost:' + port);
 
-
-// connect to database
 mongoose.connect(config.database);
 
-// pass users' API
 apiRoutes(app);
-// pass stores' API
 storesApiRoutes(app);
 
-// exporting the app 
 module.exports = app;
