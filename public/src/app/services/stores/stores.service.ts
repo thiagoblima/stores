3b19d3e4396371a3f7352cb0a095079cc0ac45c0
Thiago <thiagobr@br.ibm.com>
2017-05-19 01:08:11 -0300

    Creating Stores REST API A033

---
+++ public/src/app/services/stores/stores.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { StoresData } from '../../models/index';
+
+@Injectable()
+export class StoresService {
+    constructor(private http: Http) { }
+
+    getAll() {
+        return this.http.get('/api/stores', this.jwt()).map((response: Response) => response.json());
+    }
+
+    getById(_id: any) {
+        return this.http.get('/api/store/' + _id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    update(store: StoresData) {
+        return this.http.put('/api/user/' + store._id, store, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(_id: number) {
+        return this.http.delete('/api/store/' + _id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    private jwt() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
+    }
+}
