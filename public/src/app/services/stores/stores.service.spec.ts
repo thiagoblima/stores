27555ff9972eff7d4ba866edf0fd44bb210171ff
Thiago <thiagobr@br.ibm.com>
2017-05-19 01:07:51 -0300

    Creating Stores REST API A032

---
+++ public/src/app/services/stores/stores.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { StoresService } from './stores.service';
+
+describe('StoresService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [StoresService]
+    });
+  });
+
+  it('should ...', inject([StoresService], (service: StoresService) => {
+    expect(service).toBeTruthy();
+  }));
+});
