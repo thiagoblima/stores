6fa4b8b7cd2fd9a1032f57f566b560afd5861390
Thiago <thiagobr@br.ibm.com>
2017-05-19 23:31:47 -0300

    Creating Welcome Component A023

---
+++ public/src/app/models/stores.ts
@@ -0,0 +1,28 @@
+/**
+ * @author: Thiago Lima
+ * 
+ * @description: Generic class which's used to 
+ * define its typings. It's being imported in 
+ * { StoresService }
+ * 
+ */
+
+interface StoresSettings<T, Z> {
+
+    _id: String;
+    store_name: String;
+    store_image: String;
+    store_phone: Number;
+    store_address: String;
+    
+}
+
+export class Stores implements StoresSettings<String, Number> {
+
+    public _id;
+    public store_name;
+    public store_image;
+    public store_phone;
+    public store_address;
+
+}
