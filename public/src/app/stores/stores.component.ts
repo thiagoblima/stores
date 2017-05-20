0a07665ff833f1a3a251a7ccb8a5b7ac72a7ba72
Thiago <thiagobr@br.ibm.com>
2017-05-20 02:59:38 -0300

    Stores Component A016

--- public/src/app/stores/stores.component.ts
+++ public/src/app/stores/stores.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit, trigger, transition, style, animate, state } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/index';
+import { Stores } from '../models/index';
+import { UserService } from '../services/auth/index';
+import { StoresService } from '../services/stores/index';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-stores',
+  animations: [
+    trigger(
+      'myAnimation',
+      [
+        transition(
+          ':enter', [
+            style({ backgroundColor: 'transparent', color: '#F4F2F4', opacity: 1 }),
+            animate('800ms')
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({ opacity: 0 }),
+            animate('800ms')
+          ]
+        )
+      ]
+    )
+  ],
   templateUrl: './stores.component.html',
   styleUrls: ['./stores.component.scss']
 })
+
 export class StoresComponent implements OnInit {
 
-  constructor() { }
+  private currentUser: User;
+  private stores: Stores[] = [];
+  private message: string = '';
+
+  constructor(private router: Router, private storesService: StoresService, private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  private getStores() {
+    this.storesService.getStores().subscribe(stores => { this.stores = stores; });
+  }
+
+  private getUserInfo() {
+    this.userService.getUserInfo().subscribe(message => { this.message = message; })
+  }
 
   ngOnInit() {
+
+    this.getStores();
+    this.getUserInfo();
+
   }
+
 
 }
