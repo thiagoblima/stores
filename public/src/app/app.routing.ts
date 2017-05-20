82e94bdb43f82c89181b27c9b64d27c7a136f4c1
Thiago <thiagobr@br.ibm.com>
2017-05-20 00:38:16 -0300

    Stores Component A007

--- public/src/app/app.routing.ts
+++ public/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/index';
 import { SettingsComponent } from './settings/index';
+import { StoresComponent } from './stores/index';
 import { LoginComponent } from './login/index';
 import { UserComponent } from './settings/user/index';
 import { RegisterComponent } from './register/index';
@@ -11,6 +12,7 @@ const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
     { path: 'api/user/:id', component: UserComponent, canActivate: [AuthGuard] },
+    { path: 'stores', component: StoresComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
  
