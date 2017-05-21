fa9605fa12d8531707c0815cef8b3cca65707b3b
Thiago <thiagobr@br.ibm.com>
2017-05-21 17:08:05 -0300

    Stores ByID REST API A018

--- public/src/app/models/stores.ts
+++ public/src/app/models/stores.ts
@@ -7,24 +7,28 @@
  * 
  */
 
-interface StoresSettings<T, Z> {
+interface StoresSettings <T, Z> {
 
     _id: String;
     store_name: String;
     store_image: String;
     store_phone: Number;
     store_country: String;
+    store_city: String,
+    store_type: String,
     store_address: String;
     
 }
 
-export class Stores implements StoresSettings<String, Number> {
+export class Stores implements StoresSettings <String, Number> {
 
     public _id;
     public store_name;
     public store_image;
     public store_phone;
     public store_country;
+    public store_city;
+    public store_type;
     public store_address;
 
 }
