2347be9447aba686d8452da52d0ea66b2c6eb0bb
Thiago <thiagobr@br.ibm.com>
2017-05-27 02:21:14 -0300

    Adding File Upload A009

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const config = require('./config/database'); 
+const utilApiRoutes = require('./controllers/utilApi');
 const authApiRoutes = require('./controllers/authApi');
 const userApiRoutes = require('./controllers/userApi');
 const storesApiRoutes = require('./controllers/storesApi');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -13,12 +15,15 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(fileUpload());
 
 app.use('/', express.static(__dirname + '/public/dist'));
 
 app.get('/', (req, res) => {
     res.send('The API is at http://localhost:' + port + '/api');
 });
+
+
 
 app.use(morgan('dev'));
 
@@ -27,6 +32,8 @@ console.log('App running at: http://localhost:' + port);
 
 mongoose.connect(config.database);
 
+
+utilApiRoutes(app);
 authApiRoutes(app);
 userApiRoutes(app);
 storesApiRoutes(app);
