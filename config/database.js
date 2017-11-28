0912876d7d9d3006db0a8815af2c84e7840d590b
Thiago Lima <thitheguy@gmail.com>
2017-11-28 18:34:56 -0200

    Adding info for the database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,10 @@
+/**
+ * @author: Thiago Lima <thiagolimasp@live.com>
+ * @description: here we have the secret and the database instances
+ * being exported to become available in other files.
+ * @module: App { server }
+ */
+
 module.exports = {
   'secret': 'recycleapisecret',
   'database': 'mongodb://localhost/stores'
