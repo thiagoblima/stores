6131ecdf7eb5135d6be53df1085bb0e27b493162
Thiago Lima <thitheguy@gmail.com>
2017-12-20 01:35:28 -0200

    Review and improvements on update store

--- models/otpdb.js
+++ models/otpdb.js
@@ -1,31 +1,28 @@
-// grab the things we need
+/**
+ * @author     : <thiagolimasp@live.com> Thiago Lima
+ * @module     : App { server }
+ * @description: optdb's model object.
+ */
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create a schema
 let OTPschema = new Schema({
   otp: Number,
   otp_number: Number,
   created_at: Date
 });
 
-
-// on every save, add the date
 OTPschema.pre('save', (next) => {
-  // get the current date
+
   let currentDate = new Date();
 
-  // if created_at doesn't exist, add to that field
   if (!this.created_at)
     this.created_at = currentDate;
 
   next();
 });
 
-
-// the schema is useless so far
-// we need to create a model using it
 let otpDB = mongoose.model('otpDB', OTPschema);
 
-// make this available to our users in our Node applications
 module.exports = otpDB;
