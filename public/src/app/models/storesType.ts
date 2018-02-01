71aaafaf23eaa0f8b421bae72d13d39de929983c
Thiago Lima <thitheguy@gmail.com>
2018-02-01 21:12:30 -0200

    code review

--- public/src/app/models/storesType.ts
+++ public/src/app/models/storesType.ts
@@ -1,26 +1,20 @@
 /**
- * @author: Thiago Lima
- * 
- * @description: Generic class which's used to 
- * define its typings. It's being imported in 
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @description: Generic class which's used to
+ * define its typings. It's being imported in
  * { StoresType }
- * 
  */
 
 interface StoresTypeSettings<T> {
-
-    _id: String;
-    type: String;
-    description: String;
-    logo: String;
-    
+  _id: String;
+  type: String;
+  description: String;
+  logo: String;
 }
 
 export class StoresType implements StoresTypeSettings<String> {
-
-    public _id;
-    public type;
-    public description;
-    public logo;
-
+  public _id;
+  public type;
+  public description;
+  public logo;
 }
