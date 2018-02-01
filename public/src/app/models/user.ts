492d5d5c021fe39175b91a8e5790288023e05c0f
Thiago Lima <thitheguy@gmail.com>
2018-02-01 21:10:48 -0200

    code review

--- public/src/app/models/user.ts
+++ public/src/app/models/user.ts
@@ -1,32 +1,28 @@
 /**
- * @author: Thiago Limaall
- * 
- * @description: Generic class which's used to 
- * define its typings. It's being imported in 
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @description: Generic class which's used to
+ * define its typings. It's being imported in
  * { UserService }
- * 
  */
 
-interface UserData <T, Z> {
-
-    _id: string;
-    username: string;
-    password: string;
-    email: string;
-    firstname: string;
-    lastname: string;
-    age: number;
-    file: string;
-    
+interface UserData<T, Z> {
+  _id: string;
+  username: string;
+  password: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  age: number;
+  file: string;
 }
 
-export class User implements UserData <String, Number> {
-    public _id;
-    public username;
-    public password;
-    public email;
-    public firstname;
-    public lastname;
-    public age;
-    public file;
+export class User implements UserData<String, Number> {
+  public _id;
+  public username;
+  public password;
+  public email;
+  public firstname;
+  public lastname;
+  public age;
+  public file;
 }
