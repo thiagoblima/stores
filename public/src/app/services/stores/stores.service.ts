30e82054e9bbdb5f6dac1ec74f98c936530b7ad1
Thiago Lima <thitheguy@gmail.com>
2018-02-01 21:26:49 -0200

    code review

--- public/src/app/services/stores/stores.service.ts
+++ public/src/app/services/stores/stores.service.ts
@@ -4,36 +4,46 @@ import { Store } from '../../models/index';
 
 @Injectable()
 export class StoresService {
-    constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
-    createStore(store: Store) {
-        return this.http.post('/api/stores', store, this.jwt()).map((response: Response) => response.json());
+  createStore(store: Store) {
+    return this.http
+      .post('/api/stores', store, this.jwt())
+      .map((response: Response) => response.json());
+  }
+
+  getStores() {
+    return this.http
+      .get('/api/stores', this.jwt())
+      .map((response: Response) => response.json());
+  }
+
+  getById(_id: any) {
+    return this.http
+      .get("/api/store/" + _id, this.jwt())
+      .map((response: Response) => response.json());
+  }
+
+  update(store: Store) {
+    return this.http
+      .put('/api/store/' + store._id, store, this.jwt())
+      .map((response: Response) => response.json());
+  }
+
+  delete(_id: number) {
+    return this.http
+      .delete('/api/store/' + _id, this.jwt())
+      .map((response: Response) => response.json());
+  }
+
+  // private helper methods
+
+  private jwt() {
+    // create authorization header with jwt token
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
+      let headers = new Headers({ Authorization: currentUser.token });
+      return new RequestOptions({ headers: headers });
     }
-
-    getStores() {
-        return this.http.get('/api/stores', this.jwt()).map((response: Response) => response.json());
-    }
-
-    getById(_id: any) {
-        return this.http.get('/api/store/' + _id, this.jwt()).map((response: Response) => response.json());
-    }
-
-    update(store: Store) {
-        return this.http.put('/api/store/' + store._id, store, this.jwt()).map((response: Response) => response.json());
-    }
-
-    delete(_id: number) {
-        return this.http.delete('/api/store/' + _id, this.jwt()).map((response: Response) => response.json());
-    }
-
-    // private helper methods
-
-    private jwt() {
-        // create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
-    }
+  }
 }
