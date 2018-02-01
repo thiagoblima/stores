e7f41d119349f4420514075944a0b798f30ff105
Thiago Lima <thitheguy@gmail.com>
2018-02-01 21:18:17 -0200

    code review

--- public/src/app/models/stores.ts
+++ public/src/app/models/stores.ts
@@ -1,36 +1,30 @@
 /**
- * @author: Thiago Lima
- * 
- * @description: Generic class which's used to 
- * define its typings. It's being imported in 
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @description: Generic class which's used to
+ * define its typings. It's being imported in
  * { StoresService }
- * 
  */
 
-interface StoresSettings <T, Z, Y> {
-
-    _id: String;
-    store_name: String;
-    store_image: String;
-    store_phone: Number;
-    store_country: String;
-    store_city: String,
-    store_type: String,
-    store_address: String;
-    updated_at: Date
-    
+interface StoresSettings<T, Z, Y> {
+  _id: String;
+  store_name: String;
+  store_image: String;
+  store_phone: Number;
+  store_country: String;
+  store_city: String;
+  store_type: String;
+  store_address: String;
+  updated_at: Date;
 }
 
-export class Store implements StoresSettings <String, Number, Date> {
-
-    public _id;
-    public store_name;
-    public store_image;
-    public store_phone;
-    public store_country;
-    public store_city;
-    public store_type;
-    public store_address;
-    public updated_at;
-
+export class Store implements StoresSettings<String, Number, Date> {
+  public _id;
+  public store_name;
+  public store_image;
+  public store_phone;
+  public store_country;
+  public store_city;
+  public store_type;
+  public store_address;
+  public updated_at;
 }
