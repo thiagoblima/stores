967f51b2112d366d8fdd4863fcd5c29a7b3fd22e
Thiago Lima <thitheguy@gmail.com>
2018-02-01 21:09:58 -0200

    code review

--- public/src/app/app.routing.ts
+++ public/src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/index';
+import { AboutComponent } from './about/index';
 import { SettingsComponent } from './settings/index';
 import { StoresComponent } from './stores/index';
 import { StoreComponent } from './stores/store/index';
@@ -11,15 +12,16 @@ import { AuthGuard } from './services/guards/index';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
-    { path: 'api/user/:id', component: UserComponent, canActivate: [AuthGuard] },
+    { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
     { path: 'stores', component: StoresComponent, canActivate: [AuthGuard]},
     { path: 'api/store/:id', component: StoreComponent, canActivate: [AuthGuard] },
+    { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+    { path: 'api/user/:id', component: UserComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
- 
+
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
- 
+
 export const routing = RouterModule.forRoot(appRoutes);
