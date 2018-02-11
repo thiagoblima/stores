6484496517cdf3b4955b5efcd7f73e470a462a58
Thiago Lima <thitheguy@gmail.com>
2018-02-11 01:06:39 -0200

    Util REST API (file uploader endpoints)

--- controllers/utilApi.js
+++ controllers/utilApi.js
@@ -1,91 +1,77 @@
 /**
  * @author     : Thiago Lima <thiagolimasp@live.com>
  * @module     : App { server }
- * @description: Here goes the REST API responsible for 
+ * @description: Here goes the REST API responsible for
  * uploading files, properly used for saving photos or pdf files.
  */
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const config = require('../config/database');
-const User = require('../models/user');
-const Stores = require('../models/stores');
-const jwt = require('jwt-simple');
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const config = require("../config/database");
+const User = require("../models/user");
+const Stores = require("../models/stores");
+const jwt = require("jwt-simple");
 
 app.use(passport.initialize());
 app.use(fileUpload());
 
-require('../config/passport')(passport);
+require("../config/passport")(passport);
 
+module.exports = app => {
+  const utilApiRoutes = express.Router();
 
-module.exports = (app) => {
+  // connect the api routes under /api/*
+  app.use("/api", utilApiRoutes);
 
-    const utilApiRoutes = express.Router();
+  /**
+   * @function: getToken()
+   * @param: headers
+   * @prop: authorization
+   * @description: getting headers for JWT token
+   */
 
-    // connect the api routes under /api/*
-    app.use('/api', utilApiRoutes);
+  getToken = headers => {
+    if (headers && headers.authorization) {
+      let parted = headers.authorization.split(" ");
+      if (parted.length === 2) {
+        return parted[1];
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
 
-    /**
-     * @function: getToken()
-     * @param: headers
-     * @prop: authorization
-     * @description: getting headers for JWT token
-     */
+  // user asset uploder
+  utilApiRoutes.post("/upload/user/asset", (req, res) => {
+    if (!req.files) return res.status(400).send("No files were uploaded.");
 
+    let file = req.files.file;
 
-    getToken = (headers) => {
-        if (headers && headers.authorization) {
-            let parted = headers.authorization.split(' ');
-            if (parted.length === 2) {
-                return parted[1];
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    };
+    file.mv("./public/dist/assets/images/user/" + req.files.file.name, err => {
+      if (err) return res.status(500).send(err);
 
-
-    utilApiRoutes.post('/upload/user/asset', (req, res) => {
-
-        if (!req.files)
-            return res.status(400).send('No files were uploaded.');
-
-        let file = req.files.file;
-
-        file.mv('./public/dist/assets/images/user/' + req.files.file.name, (err) => {
-            if (err)
-                return res.status(500).send(err);
-
-            res.send('File uploaded!');
-            console.log(req.files.file);
-        });
-
+      res.send("File uploaded!");
+      console.log(req.files.file);
     });
+  });
 
-    utilApiRoutes.post('/upload/store/asset', (req, res) => {
+  // store asset uploader
+  utilApiRoutes.post("/upload/store/asset", (req, res) => {
+    if (!req.files) return res.status(400).send("No files were uploaded.");
 
-        if (!req.files)
-            return res.status(400).send('No files were uploaded.');
+    let file = req.files.file;
 
-        let file = req.files.file;
+    file.mv("./public/dist/assets/images/store/" + req.files.file.name, err => {
+      if (err) return res.status(500).send(err);
 
-        file.mv('./public/dist/assets/images/store/' + req.files.file.name, (err) => {
-            if (err)
-                return res.status(500).send(err);
-
-            res.send('File uploaded!');
-            console.log(req.files.file);
-        });
-
+      res.send("File uploaded!");
+      console.log(req.files.file);
     });
-
-
+  });
 };
- 
