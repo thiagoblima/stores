6d659a07ffb0c388d9c0884e4de7c22c206eb8fc
Thiago Lima <thitheguy@gmail.com>
2018-02-11 00:59:38 -0200

    Mongoose & REST API Promises

--- server.js
+++ server.js
@@ -4,36 +4,37 @@
  * @description: Main Server Node Application.
  */
 
-const config = require('./config/database'); 
-const utilApiRoutes = require('./controllers/utilApi');
-const authApiRoutes = require('./controllers/authApi');
-const userApiRoutes = require('./controllers/userApi');
-const storesApiRoutes = require('./controllers/storesApi');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const config = require("./config/database");
+const utilApiRoutes = require("./controllers/utilApi");
+const authApiRoutes = require("./controllers/authApi");
+const userApiRoutes = require("./controllers/userApi");
+const storesApiRoutes = require("./controllers/storesApi");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-app.use('/', express.static(__dirname + '/public/dist'));
+app.use("/", express.static(__dirname + "/public/dist"));
 
-app.get('/', (req, res) => {
-    res.send('The API is at http://localhost:' + port + '/api');
+app.get("/", (req, res) => {
+  res.send("The API is at http://localhost:" + port + "/api");
 });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.listen(port);
-console.log('App running at: http://localhost:' + port);
+console.log("App running at: http://localhost:" + port);
 
-mongoose.connect(config.database, {useMongoClient: true});
+mongoose.Promise = global.Promise;
+const promise = mongoose.connect(config.database, { useMongoClient: true });
+promise.then(connect => connect).catch(err => err);
 
 /**
  * @function: utilApiRoutes, authApiRoutes, userApiRoutes, storesApiRoutes
@@ -46,4 +47,3 @@ userApiRoutes(app);
 storesApiRoutes(app);
 
 module.exports = app;
- 
