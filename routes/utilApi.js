0bfc8f8dd80091de6cd132fb10afe0a093554bcd
Thiago Lima <thitheguy@gmail.com>
2018-03-24 22:37:53 -0300

    Adding a new token function file

--- routes/utilApi.js
+++ routes/utilApi.js
@@ -55,23 +55,3 @@ module.exports = app => {
     });
   });
 };
-
-/**
- * @function: getToken()
- * @param: headers
- * @prop: authorization
- * @description: getting headers for JWT token
- */
-
-module.exports = getToken = headers => {
-  if (headers && headers.authorization) {
-    let parted = headers.authorization.split(" ");
-    if (parted.length === 2) {
-      return parted[1];
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-};
