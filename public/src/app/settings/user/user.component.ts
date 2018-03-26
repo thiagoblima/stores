4dcda40fac62173b637fdc67ecc11285ae6c0a1d
Thiago Lima <thitheguy@gmail.com>
2018-03-26 00:27:59 -0300

    Modifing relative path

--- public/src/app/settings/user/user.component.ts
+++ public/src/app/settings/user/user.component.ts
@@ -109,7 +109,7 @@ export class UserComponent implements OnInit {
       let options = new RequestOptions({ headers: headers });
 
       this.model.file = file.name;
-      this.model.path = '../../assets/images/user/';
+      this.model.path = './assets/images/user/';
 
       this.userService.fileChange(formData, options).subscribe(
         data => {
