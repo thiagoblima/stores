446b73bd76ab49567ec480b2d3971a20207c2a6b
Thiago Lima <thitheguy@gmail.com>
2018-03-31 19:07:42 -0300

    Removing ngBootstrap module

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './commons/nav/nav.component';
@@ -18,13 +17,12 @@ import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './services/guards/index';
 import { AlertService } from './services/auth/index';
 import { StoresService } from './services/stores/stores.service';
-import { AuthenticationService, UserService, } from './services/auth/index';
+import { AuthenticationService, UserService } from './services/auth/index';
 import { SettingsComponent } from './settings/settings.component';
 import { UserComponent } from './settings/user/user.component';
 import { StoresComponent } from './stores/stores.component';
 import { StoreComponent } from './stores/store/store.component';
 import { AboutComponent } from './about/about.component';
-
 
 @NgModule({
   declarations: [
@@ -43,7 +41,6 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    NgbModule.forRoot(),
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
@@ -60,4 +57,4 @@ import { AboutComponent } from './about/about.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
