412b3cc3166d236b8eeefeba15356f55253f5d08
Thiago Lima <thitheguy@gmail.com>
2018-04-29 13:51:57 -0300

    Importing UserConfig and ErrorConfig interfaces to the User class

--- public/src/app/settings/user/user.component.ts
+++ public/src/app/settings/user/user.component.ts
@@ -9,32 +9,10 @@ import { NavComponent } from '../../commons/nav/nav.component';
 import { HeaderComponent } from '../../commons/header/header.component';
 import { FooterComponent } from '../../commons/footer/footer.component';
 import { WelcomeComponent } from '../../welcome/welcome.component';
-import { User } from '../../models/index';
+import { User, UserConfig, ErrorConfig } from '../../models/index';
 import { UserService, AlertService } from '../../services/auth/index';
 import 'rxjs/add/operator/switchMap';
 
-interface UserConfig<T, X, Y, Z> {
-  users: Array<{}>;
-  model: Object;
-  error: string;
-  loading: string;
-  getCurrentUser(): Object;
-  getMessage(): string;
-  getShow(): boolean;
-}
-
-interface ErrorConfig<T, X, Y> {
-  success: boolean;
-  msg: string;
-  err: {
-    name: string,
-    message: string,
-    ok: number,
-    errmsg: string,
-    code: number,
-    codeName: string
-  }
-}
 @Component({
   moduleId: module.id,
   selector: 'app-user',
